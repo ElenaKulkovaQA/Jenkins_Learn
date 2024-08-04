83d9d398432f0f6e30405527a2f02f311f18791c
Elena <elena.regiionh@gmail.com>
2024-08-04 16:39:19 +0300

    add config

--- src/test/java/tests/StepRegistrationFormWithPageObjectsTest.java
+++ src/test/java/tests/StepRegistrationFormWithPageObjectsTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import pages.components.CheckResultComponent;
 import pages.RegistrationPage;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.sleep;
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,9 +7,7 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import java.util.Map;
-
 import utils.Attachments;
 
 public class TestBase {
